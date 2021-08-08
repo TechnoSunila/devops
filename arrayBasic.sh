abf6ce285894a7de10a98c0c107f53fc5858db12
techsunil <sunilambekarmca@gmail.com>
2021-08-08 21:06:18 +0530

    array iteration code has been added

--- arrayBasic.sh
+++ arrayBasic.sh
@@ -1,2 +1,22 @@
 #!/bin/bash
 # Array basics provided here .........................................
+
+
+
+# 1. delcaring array 
+
+read -p "Enter the Arr : " -a ARR
+
+# 3. Accessing array Elements ( directly accessign values)
+printf ${#ARR[@]}
+for i in ${ARR[@]}; do
+    printf "$i \n"
+done
+
+#4. Accessing array Elements ( Using Index)
+printf "${#ARR[@]} \n"
+for((i=0;i<${#ARR[@]};i++)); do
+    printf "${ARR[i]}\t"
+done
+
+ 
